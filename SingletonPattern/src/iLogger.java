6ba6c4ee393f6ddcd959761104e4c4e6e07e74c4
sunil <svrajput@hotmail.com>
2018-07-17 13:38:19 -0500

    note added for volatile keyword

--- SingletonPattern/src/iLogger.java
+++ SingletonPattern/src/iLogger.java
@@ -1,11 +1,18 @@
 public class iLogger  {
-	
-	//private static final Long serialVersionUID = 1L;
-	 
+		 
 	private int i = 10;
 	
-	// create instance variable. 
-    private volatile static iLogger instance = null;
+	/* 
+	 * Please be sure to use “volatile” keyword with instance variable otherwise you can 
+	 * run into an out of order write error scenario, where reference of an instance is 
+	 * returned before actually the object is constructed i.e. JVM has only allocated the 
+	 * memory and constructor code is still not executed. In this case, your other thread, 
+	 * which refer to uninitialized object may throw null pointer exception and can even 
+	 * crash the whole application.
+	 * 
+	 * create instance variable
+	*/ 
+	private volatile static iLogger instance = null;
 		
 	// static method to return instance object
 	public static iLogger getInstance() {
